dffe640600ca47fbea26f055c2b9b9399943b5ac
Charles HUSQUIN <charles@mothership.local>
2020-03-27 19:50:28 -0700

    test configuration ok

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,15 @@
-module.export = {
+module.exports = {
   roots: ['<rootDir>'],
   transform: {
     '\\.(js|jsx)?$': 'babel-jest',
   },
   moduleNameMapper: {
-    "^.+\\.(css|scss)$": '<rootDir>/tests/config/scsStub.js'
+    "^.+\\.(css|scss)$": '<rootDir>/tests/config/scssStub.js'
   },
-  testMatch: ['<rootDir>/src/**/>(*.)test.{js, jsx}'],
+  testMatch: ['<rootDir>/tests/**/(*.)test.{js, jsx}'],
   moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
   testPathIgnorePatterns: ['/node_modules/', '/public/'],
   setupFilesAfterEnv: [
     '@testing-library/jest-dom/extend-expect', 
-    '@testing-library/react/cleanup-after-each'
   ]
 };
